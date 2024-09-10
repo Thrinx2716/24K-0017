2e4cb212ddc2e2d4c3ccbf4e3eac838a2223ddeb
Salman A. <wanderingconjuror219696@gmail.com>
2024-09-10 10:37:05 +0500

    Update Q1.c

--- Lab Assignments/Lab 4/Q1.c
+++ Lab Assignments/Lab 4/Q1.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main(){ // start main
+	int Num;
+	printf("Enter number: ");
+	scanf("%d", &Num);
+	if (Num % 2 == 0){
+		printf("%d is Even", Num);
+	}
+	else{
+		printf("%d is Odd", Num);
+	}
+return 0;
+} // end main
