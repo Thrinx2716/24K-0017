f76972ef8efd4d8805d0988f6f169f3412a3bfbb
Salman A. <wanderingconjuror219696@gmail.com>
2024-10-03 02:48:09 +0500

    Update Q7.c

--- Lab Assignments/Lab 6/Q7.c
+++ Lab Assignments/Lab 6/Q7.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main() { // Start Main
-	int Number, Sum, OriginalNum, Remainder, n = 0, i, Power;
+    int Number, Sum, OriginalNum, Remainder, n = 0, i, Power;
+
+    // Input
     printf("Enter a number: ");
     scanf("%d", &Number);
 
+    // Process
     OriginalNum = Number;
     while (OriginalNum != 0){
         OriginalNum /= 10;
@@ -15,16 +18,15 @@ int main() { // Start Main
 
     while (OriginalNum != 0) {
         Remainder = OriginalNum % 10;
-
         Power = 1;
         for (i = 0; i < n; i++) {
             Power *= Remainder;
         }
-
         Sum += Power;
         OriginalNum /= 10;
     }
 
+    // Output
     (Sum == Number) ? printf("%d is an Armstrong Number.", Number) : printf("%d is not an Armstrong Number.", Number);
 
 	return 0;
