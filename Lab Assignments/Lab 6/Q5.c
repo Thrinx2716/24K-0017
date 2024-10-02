655e6522c016cbc0331339c7adf372cbb90ff196
Salman A. <wanderingconjuror219696@gmail.com>
2024-10-03 02:52:00 +0500

    Update Q5.c

--- Lab Assignments/Lab 6/Q5.c
+++ Lab Assignments/Lab 6/Q5.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 
 int main(){ // Start Main
-    int Number, i, Is_Prime = 1;
+        int Number, i, Is_Prime = 1;
 
-    // Input
-    printf("Enter a number: ");
-    scanf("%d", &Number);
+        // Input
+        printf("Enter a number: ");
+        scanf("%d", &Number);
 
-    // Process
-    if (Number <= 1) {
-        Is_Prime = 0;
-    } else {
-        for (i = 2; i * i <= Number; i++) {
-            if (Number % i == 0) {
-                Is_Prime = 0;
-                break;
+        // Process
+        if (Number <= 1) {
+            Is_Prime = 0;
+        } else {
+            for (i = 2; i * i <= Number; i++) {
+                if (Number % i == 0) {
+                    Is_Prime = 0;
+                    break;
+                }
             }
         }
-    }
 
-    // Output
-    if (Is_Prime == 1){
-        printf("%d is prime. ", Number);
-    } else {
-        printf("%d is not prime.", Number);
-    }
+        // Output
+        if (Is_Prime == 1){
+            printf("%d is prime. ", Number);
+        } else {
+            printf("%d is not prime.", Number);
+        }
 
-    return 0;
+        return 0;
 } // End Main
