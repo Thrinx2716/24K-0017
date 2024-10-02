461c3abd5412e0300d00b45aea2e91464470d819
Salman A. <wanderingconjuror219696@gmail.com>
2024-10-02 23:58:42 +0500

    Create Q4.c

---
+++ Lab Assignments/Lab 6/Q4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int Streak = 0;
+    int Days;
+    char Sent;
+  
+    // Input
+    printf("Enter the number of days you want to track: ");
+    scanf("%d", &Days);
+
+    // Process
+    for (int i = 1; i <= Days; i++) {
+        printf("Day %d: ", i);
+        printf("Did you send a message today? (Y/N): ");
+        scanf(" %c", &Sent);
+
+        if (Sent == 'Y' || Sent == 'y') {
+            Streak++;
+            printf("Your current streak is: %d days.\n", streak);
+        } else if (Sent == 'N' || Sent == 'n') {
+            Streak = 0;
+            printf("Streak broken and reset to 0.\n");
+        } else {
+            printf("Invalid input.\n");
+            i--;
+        }
+    }
+
+    // Output
+    printf("Your final streak is: %d days.\n", Streak);
+    return 0;
+} // End Main
