bb3b0e1d27d885f934b1673cb17ba3ff3a30981e
Salman A. <wanderingconjuror219696@gmail.com>
2024-10-03 02:51:04 +0500

    Update Q4.c

--- Lab Assignments/Lab 6/Q4.c
+++ Lab Assignments/Lab 6/Q4.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 
 int main() { // Start Main
-    int Streak = 0;
-    int Days;
-    char Sent;
+        int Streak = 0;
+        int Days;
+        char Sent;
   
-    // Input
-    printf("Enter the number of days you want to track: ");
-    scanf("%d", &Days);
+        // Input
+        printf("Enter the number of days you want to track: ");
+        scanf("%d", &Days);
 
-    // Process
-    for (int i = 1; i <= Days; i++) {
-        printf("Day %d: ", i);
-        printf("Did you send a message today? (Y/N): ");
-        scanf(" %c", &Sent);
+        // Process
+        for (int i = 1; i <= Days; i++) {
+            printf("Day %d: ", i);
+            printf("Did you send a message today? (Y/N): ");
+            scanf(" %c", &Sent);
 
-        if (Sent == 'Y' || Sent == 'y') {
-            Streak++;
-            printf("Your current streak is: %d days.\n", streak);
-        } else if (Sent == 'N' || Sent == 'n') {
-            Streak = 0;
-            printf("Streak broken and reset to 0.\n");
-        } else {
-            printf("Invalid input.\n");
-            i--;
+            if (Sent == 'Y' || Sent == 'y') {
+                Streak++;
+                printf("Your current streak is: %d days.\n", streak);
+            } else if (Sent == 'N' || Sent == 'n') {
+                Streak = 0;
+                printf("Streak broken and reset to 0.\n");
+            } else {
+                printf("Invalid input.\n");
+                i--;
+            }
         }
-    }
 
-    // Output
-    printf("Your final streak is: %d days.\n", Streak);
-    return 0;
+        // Output
+        printf("Your final streak is: %d days.\n", Streak);
+        return 0;
 } // End Main
