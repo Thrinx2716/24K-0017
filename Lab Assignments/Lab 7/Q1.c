4ed66fc1a3f534d91ca7271ed6de0609f215539c
Salman A. <wanderingconjuror219696@gmail.com>
2024-10-09 00:34:15 +0500

    Create Q1.c

---
+++ Lab Assignments/Lab 7/Q1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() { // start main
+    	int n, i, Sum = 0;
+
+    	// Input
+    	printf("Enter number of elements: ");
+    	scanf("%d", &n);
+
+    	// Process
+	int Arr[n];
+	printf("Enter the elements of the array: ");
+    	for(i = 0; i < n; i++) {
+        	scanf("%d", &Arr[i]);
+    	}
+
+	// Output
+    	for(i = 0; i < n; i++) {
+        	Sum += Arr[i];
+    	}
+
+    	printf("Sum of elements: %d\n", Sum);
+
+    	return 0;
+} // end main
