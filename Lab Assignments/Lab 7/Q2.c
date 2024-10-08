92af4ca15ce17af7bb439671938b0ecf91fe2a2a
Salman A. <wanderingconjuror219696@gmail.com>
2024-10-09 00:35:53 +0500

    Create Q2.c

---
+++ Lab Assignments/Lab 7/Q2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() { // start main
+      int n, i;
+  
+	    // Input
+	    printf("Enter number of elements: ");
+    	scanf("%d", &n);
+
+	    // Process
+    	int Arr[n];
+	    printf("Enter elements of the array: ");
+    	for(i = 0; i < n; i++) {
+        	scanf("%d", &Arr[i]);
+    	}
+	
+	    // Output
+    	printf("Elements in reverse order: ");
+    	for(i = n - 1; i >= 0; i--) {
+        	printf("%d ", Arr[i]);
+    	}
+
+    	return 0;
+} // end main
